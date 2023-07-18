0b7d131f90225de95adfa9a0663d3537b29d50d8
Jesse He <hjesse92@gmail.com>
2023-07-17 18:09:18 -0700

    initial commit for building out in Flask

---
+++ app.py
@@ -0,0 +1,27 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+   index_file="index.html"
+   return render_template(index_file)
+
+@app.route("/department-job")
+def department_job():
+    department_job_file="department-job.html"
+    return render_template(department_job_file)
+
+@app.route("/overtime-pay")
+def overtime_pay():
+    overtime_pay_file="overtime-pay.html"
+    return render_template(overtime_pay_file)
+
+@app.route("/city-comparison-dashboard")
+def city_comparison_dashboard():
+    city_comparison_dashboard_file="city-comparison-dashboard.html"
+    return render_template(city_comparison_dashboard_file)
+
+if __name__ == "__main__":
+   app.run(debug=True)
