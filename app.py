f46e25bb4959d3f747cce2863e40f726d6cb18c1
Jesse He <hjesse@fb.com>
2023-07-18 01:29:28 -0700

    added changes for css and index

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def index():
    index_file="index.html"
    return render_template(index_file)
